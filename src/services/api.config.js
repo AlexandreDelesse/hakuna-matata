d2f12e5aed0660db146c251d598f85efe5ab518f
AlexandreDelesse <alex.delesse01@gmail.com>
2021-10-30 23:12:45 +0200

    change api IP

--- src/services/api.config.js
+++ src/services/api.config.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.API_URL || 'http://15.236.39.1:5001/api',
+  baseURL: process.env.API_URL || 'http://13.38.2.217:5001/api',
   timeout: 1000,
 })
 
-// 15.236.39.1
+// 13.38.2.217
